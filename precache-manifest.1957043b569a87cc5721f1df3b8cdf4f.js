e1ad6ae8a1361dcb07abd4b69b5ed76105d937c5
Ryan Zhou <ryanzhou7@gmail.com>
2020-04-04 20:28:12 -0700

    Updates

---
+++ precache-manifest.1957043b569a87cc5721f1df3b8cdf4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b067f539f164fb3a7ef8f7577987f78e",
+    "url": "/create-react-plus/index.html"
+  },
+  {
+    "revision": "dceb82fa4a717b49dc74",
+    "url": "/create-react-plus/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "03f1bcd37d4560cc8305",
+    "url": "/create-react-plus/static/js/2.7d4770a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/create-react-plus/static/js/2.7d4770a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dceb82fa4a717b49dc74",
+    "url": "/create-react-plus/static/js/main.df333b87.chunk.js"
+  },
+  {
+    "revision": "667671444634a20b445e",
+    "url": "/create-react-plus/static/js/runtime-main.54b61970.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/create-react-plus/static/media/logo.5d5d9eef.svg"
+  }
+]);
